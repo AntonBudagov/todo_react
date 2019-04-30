515da4c50b913ea063ce21af4a95cf59c3001ac7
Budagov Anton <a.budagov@artkai.ai>
2019-04-30 21:24:44 +0300

    ~refactoring: class component to functional components;

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,71 +1,32 @@
-import React, {Component, useState , useEffect} from "react";
+import React, {useState , useEffect} from "react";
 import './search-panel.css'
 
-// const SearchPanel = () => {
-//
-//
-//   const searchText = 'Type here to search';
-//   const searchStyle = {
-//     fontSize: '20px'
-//   };
-//   return <input
-//     className="form-control search-input"
-//     type="text"
-//     placeholder={searchText}
-//     tabIndex={1}
-//     style={searchStyle}/>
-// };
-//
-// export default SearchPanel;
-// export default class SearchPanel extends Component {
-//   state = {
-//     term: '',
-//   };
-//   onChangeSearch = (event) => {
-//     const term = event.target.value
-//     this.setState({term});
-//     this.props.onSearchChange(term)
-//   }
-//
-//   render() {
-//     // const {onSearchChange} = this.props;
-//     const searchText = 'Type here to search';
-//     const searchStyle = {
-//       fontSize: '20px'
-//     };
-//   return (<input
-//     onChange={this.onChangeSearch}
-//     className="form-control search-input"
-//     type="text"
-//     placeholder={searchText}
-//     tabIndex={1}
-//     style={searchStyle}/>)
-//   }
-// }
+
 const SearchPanel = (props) => {
 
-
   const {onSearchChange} = props;
-
-  let [term, setTerm] = useState('');
+  // I
+  // let [term, setTerm] = useState('');
+  // const onChangeSearch = (event) => {
+  //   term = event.target.value;
+  //   setTerm(term);
+  //   onSearchChange(term);
+  // };
+  // II
+  const [term, setTerm] = useState('');
   const onChangeSearch = (event) => {
-    // term = ;
     setTerm(event.target.value);
-    // onSearchChange(term);
   };
 
   useEffect(() => {
     onSearchChange(term);
-  }, [term])
+  });
 
-
-    // const {onSearchChange} = this.props;
     const searchText = 'Type here to search';
     const searchStyle = {
       fontSize: '20px'
     };
     return (<input
-      value={term}
       onChange={onChangeSearch}
       className="form-control search-input"
       type="text"
--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,156 +1,41 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './todo-list-item.css';
-// props
-// props.label
-
-export default class TodoListItem extends Component {
-
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     done: false
-  //   }
-  // this.onLabelClick = () => {
-  //   console.log('Done:', this.props.label)
-  // }
-  // }
-  // or
-  // state = {
-  //   done: false,
-  //   important: false
-  // }
-  // onLabelClick = () => {
-  //   this.setState(({done}) => {
-  //     return {
-  //       done: !done
-  //     }
-  //   })
-  //   // console.log('Done:', this.props.label)
-  // }
-  // onMarkImportant = () => {
-  //   this.setState((state) => {
-  //     return {
-  //       important: !state.important
-  //     }
-  //   })
-  // };
-
-  render() {
-    const {label, onDeleted, onToggleDone, onToggleImportant,
-      done, important} = this.props;
-    // const {done, important} = this.state;
-    let classNames = 'todo-list-item';
-
-    if (done) classNames += ' done';
-    if (important) classNames += ' important';
-
-    return (<div>
-        <span className={classNames}>
-          <span className="todo-list-item-label"
-                onClick={onToggleDone}>
-            {label}
-          </span>
-          </span>
-        {/*this.onLabelClick.bind(this)*/}
-        <button type="button"
-                onClick={onDeleted}
-                className="btn btn-outline-danger btn-sm float-right">
-          <i className="fa fa-trash-o"></i>
-        </button>
-        <button type="button"
-                onClick={onToggleImportant}
-                className="btn btn-outline-success btn-sm float-right">
-          <i className="fa fa-exclamation"></i>
-        </button>
-      </div>
-    )
-  }
-}
-//     // constructor() {
-//     //     super();
-//     //
-//     //     this.onLabelClick = () => {
-//     //         console.log('Done:', this.props.label)
-//     //     }
-//     //     this.state = {
-//     //     done: false
-//     // }
-//     // or
-//     state = {
-//         done: false,
-//         important: false
-//     };
-//     onLabelClick = () => {
-//         // console.log('Done:', this.props.label)
-//         this.setState(({done}) => {
-//             return {
-//                 done: !done
-//             }
-//         })
-//     };
-//     onMarkImportant = () => {
-//         this.setState((state) => {
-//             return {
-//                 important: !state.important
-//             }
-//         })
-//     };
-//
-//     render() {
-//         const {label, onDeleted} = this.props;
-//         // const style = {
-//         //     color: important ? 'steelblue' : 'black',
-//         //     fontWeight: important ? 'bold' : 'normal'
-//         // };
-//         let classNames = 'todo-list-item';
-//         const {done, important} = this.state;
-//         if (done) {
-//             classNames += ' done';
-//         }
-//         if (important) {
-//             classNames += ' important';
-//         }
-//         return (
-//             <div className={classNames}>
-//                 <span onClick={this.onLabelClick} className="todo-list-item-label"> {label}</span>
-//                 <button onClick={onDeleted} type="button" className="btn btn-outline-danger btn-sm float-right">
-//                     <i className="fa fa-trash-o"></i>
-//                 </button>
-//                 <button onClick={this.onMarkImportant} type="button"
-//                         className="btn btn-outline-success btn-sm float-right">
-//                     <i className="fa fa-exclamation"></i>
-//                 </button>
-//             </div>
-//         );
-//     }
-// }
-//
-//
 
 
-// const TodoListItem = ({label, important = false}) => {
-//   const style = {
-//     color: important ? 'steelblue' : 'tomato',
-//     fontWeight: important ? 'bold' : 'normal'
-//   };
-//   let classNames = 'todo-list-item';
-//   // const {important} = this.state;
-//   if (!important) {
-//     classNames += ' done';
-//   }
-//   if (important) {
-//     classNames += ' important';
-//   }
-//   return (<div>
-//       <span className={classNames}>{label}</span>
-//       <button type="button"
-//               className="btn btn-outline-danger btn-sm float-right">
-//         <i className="fa fa-trash-o"></i>
-//       </button>
-//       <button type="button"
-//               className="btn btn-outline-success btn-sm float-right">
-//         <i className="fa fa-exclamation"></i>
-//       </button>
-//     </div>
-//   )
-// };
+const TodoListItem = (props) => {
+
+  const {
+    label,
+    onDeleted,
+    onToggleDone,
+    onToggleImportant,
+    done,
+    important
+  } = props;
+  let classNames = 'todo-list-item';
+
+  if (done) classNames += ' done';
+  if (important) classNames += ' important';
+
+  return (
+    <div>
+      <span className={classNames}>
+        <span className="todo-list-item-label"
+              onClick={onToggleDone}>
+          {label}
+        </span>
+      </span>
+      <button type="button"
+              onClick={onDeleted}
+              className="btn btn-outline-danger btn-sm float-right">
+        <i className="fa fa-trash-o"></i>
+      </button>
+      <button type="button"
+              onClick={onToggleImportant}
+              className="btn btn-outline-success btn-sm float-right">
+        <i className="fa fa-exclamation"></i>
+      </button>
+    </div>
+  )
+};
+export default TodoListItem;
